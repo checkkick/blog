fe59dbf12ec6e1eb8f3f129639b948435bce85cd
Konstantin <tuskancik226@gmail.com>
2026-05-05 16:51:42 +0300

    feat: add comments view

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404, render
+from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 
-from blog.forms import EmailPostForm
+from blog.forms import CommentForm, EmailPostForm
 
 from .models import Post
 
@@ -23,8 +24,14 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day,
     )
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
 
-    return render(request, "blog/post/detail.html", {"post": post})
+    return render(
+        request,
+        "blog/post/detail.html",
+        {"post": post, "comments": comments, "form": form},
+    )
 
 
 def post_share(request, post_id):
@@ -51,4 +58,22 @@ def post_share(request, post_id):
         request,
         "blog/post/share.html",
         {"post": post, "form": form, "sent": sent},
+    )
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+
+    return render(
+        request,
+        "blog/post/comment.html",
+        {"post": post, "form": form, "comment": comment},
     )
